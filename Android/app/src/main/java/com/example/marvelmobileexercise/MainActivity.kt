67821096cf329c15412f2bc4c6a1c14b2a570c91
SlyDaemon <apyan7852@gmail.com>
2024-02-15 00:18:31 -0800

    Cleanup

--- Android/app/src/main/java/com/example/marvelmobileexercise/MainActivity.kt
+++ Android/app/src/main/java/com/example/marvelmobileexercise/MainActivity.kt
@@ -3,17 +3,8 @@ package com.example.marvelmobileexercise
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.marvelmobileexercise.ui.screen.ComicBookInfoScreen
 import com.example.marvelmobileexercise.ui.theme.MarvelMobileExerciseTheme
-import com.example.marvelmobileexercise.viewmodel.ComicBookViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,22 +14,5 @@ class MainActivity : ComponentActivity() {
                 ComicBookInfoScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting() {
-    val viewModel: ComicBookViewModel = viewModel()
-    val test = viewModel.comicBookList.value
-    Text(
-        text = test.toString()
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MarvelMobileExerciseTheme {
-        Greeting()
     }
 }
--- Android/app/src/main/java/com/example/marvelmobileexercise/ui/screen/ComicBookInfoScreen.kt
+++ Android/app/src/main/java/com/example/marvelmobileexercise/ui/screen/ComicBookInfoScreen.kt
@@ -1,26 +1,27 @@
 package com.example.marvelmobileexercise.ui.screen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.AsyncImage
 import com.example.marvelmobileexercise.model.response.ComicBookInfoResult
-import com.example.marvelmobileexercise.ui.theme.MarvelMobileExerciseTheme
 import com.example.marvelmobileexercise.viewmodel.ComicBookViewModel
 
 @Composable
@@ -36,21 +37,39 @@ fun ComicBookInfoScreen() {
 
 @Composable
 fun ComicBookDetailScreen(comicBookInfoResult: ComicBookInfoResult) {
-    Column {
-        AsyncImage(
-            model = comicBookInfoResult.thumbnail.getImageUrl(),
-            contentDescription = "ComicBook Thumbnail Cover",
+    Column(
+        verticalArrangement= Arrangement.SpaceBetween
+    ) {
+        Column(
             modifier = Modifier
+                .verticalScroll(rememberScrollState())
+                .weight(weight = 1f, fill = false)
                 .fillMaxWidth()
-        )
-        Text(
-            text = comicBookInfoResult.title,
-            style = MaterialTheme.typography.headlineMedium
-        )
-        Text(
-            text = comicBookInfoResult.description,
-            style = MaterialTheme.typography.bodyMedium
-        )
+        ) {
+            AsyncImage(
+                model = comicBookInfoResult.thumbnail.getImageUrl(),
+                contentDescription = "ComicBook Thumbnail Cover",
+                contentScale = ContentScale.FillWidth,
+                modifier = Modifier
+                    .fillMaxWidth()
+            )
+            Text(
+                text = comicBookInfoResult.title,
+                style = MaterialTheme.typography.headlineMedium,
+                color = Color.White,
+                modifier = Modifier
+                    .background(Color.Black)
+                    .padding(horizontal = 8.dp, vertical = 10.dp)
+            )
+            Text(
+                text = comicBookInfoResult.description,
+                style = MaterialTheme.typography.bodyLarge,
+                color = Color.White,
+                modifier = Modifier
+                    .background(Color.DarkGray)
+                    .padding(horizontal = 8.dp, vertical = 5.dp)
+            )
+        }
     }
 }
 
